6a56d7f9324e280bb0b38041c12481a6cedae7d8
mat0208 <matthieuloubens@gmail.com>
2018-07-31 14:53:27 -0700

    Added build and run scripts for the Daughter Board

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt update
+apt install cmake
+## Library needed to handle GPIO I2C for Lidar sensor. This project is using the version 1.56. Visit this website to get the latest version: http://www.airspayce.com/mikem/bcm2835/
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.56.tar.gz
+tar zxvf bcm2835-1.56.tar.gz
+cd bcm2835-1.56
+./configure
+make
+make check
+make install
+cd ..
+## Build the project executables
+mkdir bin
+cd Handler/
+g++ -std=c++11 -o ../bin/main_Handler main_Handler.cpp -lpthread
+cd ../Lidar
+g++ -std=c++11 -o ../bin/main_Lidar main_Lidar.cpp -lbcm2835
+cd ../Pyranometer
+g++ -std=c++11 -o ../bin/main_Pyranometer main_Pyranometer.cpp
+cd ../WindSensor
+g++ -std=c++11 -o ../bin/main_WindSensor main_WindSensor.cpp
+cd ../OPLS
+g++ -std=c++11 -o ../bin/main_OPLS main_OPLS.cpp -lpthread
+cd ..
+chmod +x bin/*
+## Apply a patch to the kernel in order to use an older driver for i2c communication. This is required for the Lidar Garmin Lite v3  to work properly.
+echo "dtoverlay=i2c_bcm2708" >> /boot/config.txt
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/pi/DaughterBoard/bin
+./main_Handler&
+./main_Lidar&
+./main_Pyranometer&
+./main_WindSensor&
+./main_OPLS&
+
+
